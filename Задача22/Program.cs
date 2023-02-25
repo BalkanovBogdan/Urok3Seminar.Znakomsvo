1a06c36be10beaaad3e6cf3543a682fe33417425
Балканов Богдан <89509494919@mail.ru>
2023-02-25 23:56:18 +0300

    25.02.23

---
+++ Задача22/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число (N)
+// и выдает таблицу квадратов чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine()!);
+int n=1;
+int r=0;
+while(n<=N){r=n*n;n++;
+Console.Write($"{r},");}
