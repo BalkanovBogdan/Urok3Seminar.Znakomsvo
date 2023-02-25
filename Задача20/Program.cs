92ab800b6fdb8522725dec10ffc76ec8162fa7e7
Балканов Богдан <89509494919@mail.ru>
2023-02-25 23:56:03 +0300

    25.02.23

---
+++ Задача20/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит
+// расстояние между ними в 2D пространстве.
+Console.Clear();
+Console.WriteLine("Введите координаты точки A по X ");
+double x1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координаты точки A по Y");
+double y1 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координаты точки B по X");
+double x2 = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координаты точки B по Y");
+double y2 = double.Parse(Console.ReadLine()!);
+double r = Math.Sqrt(Math.Pow(x1-x2,2)+Math.Pow(y1-y2,2));
+Console.WriteLine($"Растояние между ними ={r:f2}");
