94f27b764a62228596b2692e8ea297a805ce95c0
Балканов Богдан <89509494919@mail.ru>
2023-02-25 23:55:53 +0300

    25.02.23

---
+++ Задача18/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая по заданному номеру четверти,
+// показывает диапозон возможных координат точек в этой четверти (X и Y).
+Console.Clear();
+Console.WriteLine("Введите номер четверти");
+int N=int.Parse(Console.ReadLine()!);
+if(N==1){Console.WriteLine("x>0,y>0");}
+else if(N==2){Console.WriteLine("x<0,y>0");}
+else if(N==3){Console.WriteLine("x<0,y<0");}
+else if(N==4){Console.WriteLine("x>0,y<0");}
+else if(N>4 || N<1){Console.WriteLine("Введенно не верное значение");}
