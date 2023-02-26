825f7ff86110527208b7b5def2e42f2eb3b754fb
Балканов Богдан <89509494919@mail.ru>
2023-02-26 15:13:47 +0300

    26.02

---
+++ Sem.4_Задача30/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая выводит массив из 8 элементов,
+// заполненный нулями и еденицами в случайном порядке.
+int[]array = randomnumber(8);
+//Console.WriteLine($"[{String.Join(",",array)}]");
+
+for (int i=0;i<array.Length-1;i++)
+{
+    Console.Write($"{array[i]},");
+}
+int[]randomnumber(int size)
+{
+    int[]result=new int[size];
+    for(int i=0;i<size;i++)
+    {
+        result[i]= new Random().Next(2);
+    }
+    return result;
+}    
