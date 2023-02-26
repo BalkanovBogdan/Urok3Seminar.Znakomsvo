5306a68d4a25ac8d9026547d6ef5bfdc1a6387f6
Балканов Богдан <89509494919@mail.ru>
2023-02-26 15:13:09 +0300

    26.02

---
+++ Sem.4_Задача28/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число N,
+// и выдает произведение чисел от 1 до N.
+Console.Clear();
+Console.WriteLine("Введите число.");
+int s=int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Произведение = {Proizvedenie(s)}");
+
+int Proizvedenie(int n)
+{
+   int sum=1;
+   int i=2;
+   if(n<0){i=n;n=-1;}
+   for(;i<=n;i++)
+    {sum*=i;}
+    return sum;
+}
