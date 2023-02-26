60e841409807ccbbb766680ea50d6d5614dc413e
Балканов Богдан <89509494919@mail.ru>
2023-02-26 15:12:02 +0300

    26.02

---
+++ Sem.4_ДЗ.З.25/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B.
+Console.Clear();
+Console.WriteLine("Введите два числа.");
+int a = int.Parse(Console.ReadLine()!);
+int b = int.Parse(Console.ReadLine()!);
+int sum=a;
+int i=2;
+for(;i<=b;i++)
+ {sum*=a;}
+ 
+Console.WriteLine($"Вывод={sum}");
