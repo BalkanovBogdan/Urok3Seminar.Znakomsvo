2fe4b583ade392b939b88e774fe8c5e2e8de6a78
Балканов Богдан <89509494919@mail.ru>
2023-02-26 15:12:55 +0300

    26.02

---
+++ Sem.4_Задача26/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдает количество цифр в числе.
+Console.Clear();
+Console.WriteLine("Введите число.");
+int s=int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Количество цифр = {CountNumbers(s)}");
+
+int CountNumbers(int n)
+{
+    int count =0;
+    if(n<0)
+        n*=(-1); //n=n*(-1)
+    while (n>0)
+    {
+        n/=10; //n=n/10 n=0
+        count++;
+    }  
+    return count; 
+}
