ac8bd9fc008e39ea8b40d708e5cd0798020a4391
Балканов Богдан <89509494919@mail.ru>
2023-02-26 15:12:16 +0300

    26.02

---
+++ Sem.4_ДЗ.З.27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+
+int Prompt (string message)
+{
+Console.Clear();
+Console.WriteLine("Введите число.");
+string readInput=Console.ReadLine()!;
+int r = int.Parse(readInput)!;
+return r;
+}
+int Suma(int number)
+{
+    int r=0;
+    while(number>0)
+    {
+        r+=number%10;
+        number=number/10;
+    }
+    return r;
+}
+int number=Prompt("Введите:");
+Console.WriteLine($"Сумма всех чисел в цифре{number}={Suma(number)}");
