e3705c365f8d8ee1982e48fe040a4a6a59ca0b8a
Балканов Богдан <89509494919@mail.ru>
2023-02-26 15:12:32 +0300

    26.02

---
+++ Sem.4_ДЗ.З.29/Program.cs
@@ -0,0 +1,34 @@
+﻿// Напишите программу, которая задаёт 
+// массив из 8 элементов и выводит их на экран.
+
+int Prompt(string message)
+{
+    Console.Write("Привет");
+    string ReadInput=Console.ReadLine()!;
+    int result=int.Parse(ReadInput);
+    return result;
+}
+int[] GenerateArray (int Length,int minValue,int maxValue)
+{
+    int[]array=new int[Length];
+    Random random=new Random ();
+    for(int i=0;i<Length; i++)
+    {
+        array[i]=random.Next(minValue,maxValue+1);
+    }
+}
+void PrintArray(int[]array)
+{
+    Console.Write("[");
+    for(int i=0;i<array.Length-1;i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write($"{array[array.Length-1]}");
+    Console.WriteLine("]");
+}
+int length=Prompt("Длина массива");
+int min=Prompt("Начальное значание,для диапазона случайного числа:");
+int max=Prompt("Конечное значание,для диапазона случайного числа:");
+int []array=GenerateArray(length,min,max);
+PrintArray(array);
