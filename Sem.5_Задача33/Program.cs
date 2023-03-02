7561b66cd88182bda8f93faa025bff6eabdc1ee2
Балканов Богдан <89509494919@mail.ru>
2023-03-03 02:12:34 +0300

    3.03

---
+++ Sem.5_Задача33/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте массив. Напишите программу, которая определяет,
+// присутствует ли заданное число в массиве.
+
+Console.Clear();
+Console.Write("Ведите данные массива через пробел: ");
+string elements=Console.ReadLine()!;
+int[] baseArray=GetARRAYfromString(elements!);
+Console.Write("Введите число: ");
+int n=int.Parse(Console.ReadLine()!);
+if(FindElements(baseArray, n))
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
+
+
+bool FindElements(int[]array, int el)
+{
+    foreach( var item in array)
+    {
+        if(el==item) return true;
+    }
+    return false;
+}
+
+ int [] GetARRAYfromString(string stringArray)//Новая фигня
+ {
+    string[] nums = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);//!!!!!!!!!!!
+    int[] res =new int [nums.Length];
+    for( int i=0; i< nums.Length; i++)
+    {
+        res[i]=int.Parse(nums[i]);
+    }
+    return res;
+ }
